0ee621a79795620ec271d855b38c358968546921
lsamu <lauxinyi@gmail.com>
2016-09-02 23:50:36 +0800

    更新结构

--- V5_DataCollection/Forms/Tools/frmTest.cs
+++ V5_DataCollection/Forms/Tools/frmTest.cs
@@ -96,7 +96,7 @@ namespace V5_DataCollection.Forms.Tools {
         }
 
         private void btnTestPython_Click(object sender, EventArgs e) {
-            var s = PythonExtHelper.RunPython(@"Plugins\SpiderUrl\test.py", new object[] { });
+            var s = PythonExtHelper.RunPython(@"Plugins\SpiderUrl\test.py", new object[] { "你好啊随碟附送大放送的"});
         }
     }
 }
--- V5_DataCollection/Plugins/SpiderUrl/test.py
+++ V5_DataCollection/Plugins/SpiderUrl/test.py
@@ -1,2 +1,9 @@
-﻿def start(a):
-    return "main"
+﻿def start(objects):
+    print(objects)
+    cc=''
+    for obj in objects: 
+        cc+=obj
+    print(cc)
+    return "main"+cc
+
+start(['a',"zhanghongwei"])
